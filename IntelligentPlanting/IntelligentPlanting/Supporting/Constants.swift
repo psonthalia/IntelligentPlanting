b3f0c4db3de982250411859a81962be6e97c2c30
Howard Wang <howardwang00@users.noreply.github.com>
2018-03-24 12:22:49 -0700

    Built Some UI Components

---
+++ IntelligentPlanting/IntelligentPlanting/Supporting/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  IntelligentPlanting
+//
+//  Created by Howard Wang on 3/24/18.
+//  Copyright © 2018 Howard Wang. All rights reserved.
+//
+
+
+
+struct Constants {
+    
+    static let toAR = "toAR"
+}
---
+++ IntelligentPlanting/Pods/Target Support Files/Pods-IntelligentPlanting/Pods-IntelligentPlanting-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IntelligentPlanting : NSObject
+@end
+@implementation PodsDummy_Pods_IntelligentPlanting
+@end
