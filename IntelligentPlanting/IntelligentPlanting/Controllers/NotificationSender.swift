82dd5df50ac587aac82c24f272f40b142e32b5be
Paran Sonthalia <paran.sonthalia@gmail.com>
2018-03-25 00:21:15 -0700

    Changed bundle id and plist to enable push notifications

---
+++ IntelligentPlanting/IntelligentPlanting/Controllers/NotificationSender.swift
@@ -0,0 +1,14 @@
+//
+//  NotificationSender.swift
+//  IntelligentPlanting
+//
+//  Created by Paran Sonthalia on 3/25/18.
+//  Copyright © 2018 Howard Wang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NotificationSender: UIViewController {
+    
+}
