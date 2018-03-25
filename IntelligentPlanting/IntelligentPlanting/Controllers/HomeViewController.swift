c6bda24230e5d0117c22e967b1907caa82e4ebdd
Howard Wang <howardwang00@users.noreply.github.com>
2018-03-25 02:48:01 -0700

    Removed NotificationViewController and Notification.storyboard

--- IntelligentPlanting/IntelligentPlanting/Controllers/HomeViewController.swift
+++ IntelligentPlanting/IntelligentPlanting/Controllers/HomeViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreLocation
+import FirebaseDatabase
+import UserNotifications
 
 class HomeViewController: UIViewController {
     
@@ -83,7 +85,23 @@ class HomeViewController: UIViewController {
         
         locationManager.requestLocation()
         
+        //Notifications
+        var ref: DatabaseReference!
         
+        ref = Database.database().reference().child("SID").child("111111111111")
+        
+        DispatchQueue.global(qos: .background).async {
+            ref.observe(DataEventType.value, with: { (snapshot) in
+                if let postDict = snapshot.value as? [String] {
+                    if(Int(postDict[postDict.count - 1])! > 280) {
+                        let calendar = Calendar.current
+                        let selectedDate = calendar.date(byAdding: .minute, value: 1, to: Date())
+                        let delegate = UIApplication.shared.delegate as? AppDelegate
+                        delegate?.scheduleNotification(at: selectedDate!)
+                    }
+                }
+            })
+        }
     }
 
     override func didReceiveMemoryWarning() {
