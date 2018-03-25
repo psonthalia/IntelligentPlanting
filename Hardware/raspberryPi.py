50b2349eb7486a561127e30e6912578e483b5ee9
Paran Sonthalia <paran.sonthalia@gmail.com>
2018-03-24 21:05:13 -0700

    fixed json and other

--- Hardware/raspberryPi.py
+++ Hardware/raspberryPi.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqttClient
 import time
+from firebase import firebase
 
 def on_connect(client, userdata, flags, rc):
     
@@ -16,7 +17,13 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, message):
     print "Message received: "  + message.payload
-    
+    firebase = firebase.FirebaseApplication('https://intelligent-planting-5f489.firebaseio.com', None)
+    result = firebase.get('/Moisture', None)
+    index = 0
+    firebase.post('/SID', {index: message.payload})
+    index = index + 1
+#    print result
+
 
 Connected = False   #global variable for the state of the connection
 
