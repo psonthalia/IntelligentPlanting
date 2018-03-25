51f909c6fcdd1e7a014fc56e9185f2cef4efea71
Paran Sonthalia <paran.sonthalia@gmail.com>
2018-03-25 00:30:54 -0700

    created new storyboard and started notification code

--- IntelligentPlanting/IntelligentPlanting/Controllers/NotificationSender.swift
+++ IntelligentPlanting/IntelligentPlanting/Controllers/NotificationSender.swift
@@ -8,7 +8,25 @@
 
 import Foundation
 import UIKit
+import UserNotifications
 
 class NotificationSender: UIViewController {
-    
+    override func viewDidLoad() {
+        var dateComponents = DateComponents()
+        dateComponents.hour = Int(12)
+        dateComponents.minute = Int(30)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Medication Reminder"
+        content.body = "Please take "
+        content.sound = UNNotificationSound.default()
+        let request = UNNotificationRequest(identifier: "test", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request) {(error) in
+            if let error = error {
+                print("Uh oh! We had an error: \(error)")
+            }
+        }
+    }
 }
