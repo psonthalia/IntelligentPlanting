6bb783b428c680da04aab78c2c82cf6edd398feb
Howard Wang <howardwang00@users.noreply.github.com>
2018-04-22 17:59:17 -0700

    Updated Instructions View

---
+++ IntelligentPlanting/IntelligentPlanting/Controllers/InstructionsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  InstructionsViewController.swift
+//  RoBotany
+//
+//  Created by Howard Wang on 4/22/18.
+//  Copyright © 2018 Howard Wang. All rights reserved.
+//
+
+import UIKit
+
+class InstructionsViewController: UIViewController {
+    
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nextButton.layer.cornerRadius = 5
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
